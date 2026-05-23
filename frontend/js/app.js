aed9d338433fe6e61907e2cae59f34bc9959de37
Surajt25 <tsuraj22it@student.mes.ac.in>
2026-05-23 16:56:59 +0530

    Build banking details and final submission page UI

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,11 +1,26 @@
+/*
 const employeeForm = document.getElementById("employeeForm");
 
-
-/* FORM SUBMIT */
+/* FORM SUBMIT 
 employeeForm.addEventListener("submit", function (event) {
 
     event.preventDefault();
 
     alert("Form Submitted Successfully!");
+
+});
+*/
+
+
+/* ----- Temporary submission handler for demo purposes ----- */
+const employeeForm = document.getElementById("employeeForm");
+
+/* FORM SUBMIT */
+
+employeeForm.addEventListener("submit", function (event) {
+
+    event.preventDefault();
+
+    alert("Frontend UI completed successfully!");
 
 });
